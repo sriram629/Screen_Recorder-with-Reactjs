7cfe0fd8d6c1e9a0d0c23781ae5884f50436a29d
SRI RAMA KRISHNA <127919888+sriram629@users.noreply.github.com>
2024-08-07 14:05:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     const formData = new FormData();
     formData.append("video", blob, "recording.webm");
     try {
-      await axios.post("http://localhost:5000/upload", formData, {
+      await axios.post("https://sriram629-screen-recorder-with-reactjs-zgdp.vercel.app/upload", formData, {
         headers: { "Content-Type": "multipart/form-data" },
       });
     } catch (error) {
